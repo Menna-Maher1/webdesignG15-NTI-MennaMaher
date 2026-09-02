cb62166d32348994b127756fab51de18eb7ce8c3
Menna Maher <https://github.com/Menna-Maher1>
2026-09-02 10:44:55 +0300

    assign4(js)

---
+++ session13/js/script.js
@@ -0,0 +1,31 @@
+var users =[
+{
+    name:"menna" ,
+    email:"menna@gmail"
+},
+{
+    name:"mona" ,
+    email:"mona@gmail"
+}
+];  
+
+localStorage.setItem("users", JSON.stringify(users));
+
+console.log(JSON.stringify(users))
+
+let data= JSON.parse(localStorage.getItem("users"));
+
+var containar= document.getElementById("containar");
+data.forEach(function(user){
+
+    var div = document.createElement('div')
+
+div.innerHTML= `<h2>${user.name}</h2>
+     <p>${user.email}</p>`;
+
+console.log(div)
+
+
+    containar.appendChild(div);
+})
+
